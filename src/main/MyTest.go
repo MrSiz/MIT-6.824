e7fc540cf94edd28a82bdc0a304aad0934761565
mrsiz <zhmssl@outlook.com>
2018-03-12 16:48:56 +0800

    compete wc

--- src/main/MyTest.go
+++ src/main/MyTest.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"log"
+	"hash/fnv"
+)
+
+func ihash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
 
 func main() {
 	help := make(map[string][]string)
@@ -10,4 +20,11 @@ func main() {
 	for k, v := range help {
 		fmt.Println(k, v)
 	}
+	for k := range help {
+		fmt.Println(k)
+	}
+
+	fmt.Println(ihash("he"))
+	fmt.Println(ihash("he"))
+	//log.
 }
